52f5341c81d7becba7cd7dfac7a2c72201cb2159
Hu Jiwei <guyue.jiwei@gmail.com>
2015-03-09 12:28:55 +0800

    add test case
    当传入的timestamp参数无法转换成有效时间戳时，抛出错误；

--- src/project/util/date/js/format.js
+++ src/project/util/date/js/format.js
@@ -2,6 +2,7 @@
  * string format(string $format [, int $timestamp ]);
  * 返回将整数 timestamp 按照给定的格式字串而产生的字符串
  * 参考http://php.net/manual/zh/function.date.php
+ * 针对1970年以后的日期有效
  */
 define(function (require, exports, module) {
 
--- src/project/util/date/test/format.js
+++ src/project/util/date/test/format.js
@@ -3,6 +3,17 @@ describe('Util date format module test suite', function () {
     // Wed May 09 2018 22:28:22 GMT+0800 (CST)
     var timestamp = 1525876102869;
 
+    it('pass timestamp as a error value should throw error', function (done) {
+        seajs.use('/project/util/date/js/format', function (format) {
+            expect('09').toBe(format('d', String(timestamp)));
+            expect('09').toBe(format('d', String(timestamp) + 'error'));
+            expect(function () {
+                format('d', 'error' + String(timestamp));
+            }).toThrow();
+            done();
+        });
+    });
+
     it('pass `d` should return 月份中的第几天，有前导零的 2 位数字', function (done) {
         seajs.use('/project/util/date/js/format', function (format) {
             expect('09').toBe(format('d', timestamp));
