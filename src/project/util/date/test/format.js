0bcad084ed6d7dc37a86e21d42feb6b8be66faed
Hu Jiwei <guyue.jiwei@gmail.com>
2015-03-09 12:40:10 +0800

    add test case
    `D` - 星期中的第几天，文本表示，3 个字母

--- src/project/util/date/test/format.js
+++ src/project/util/date/test/format.js
@@ -3,7 +3,7 @@ describe('Util date format module test suite', function () {
     // Wed May 09 2018 22:28:22 GMT+0800 (CST)
     var timestamp = 1525876102869;
 
-    it('pass timestamp as a error value should throw error', function (done) {
+    it('should throw error when pass timestamp as a error value', function (done) {
         seajs.use('/project/util/date/js/format', function (format) {
             expect('09').toBe(format('d', String(timestamp)));
             expect(function () {
@@ -16,9 +16,16 @@ describe('Util date format module test suite', function () {
         });
     });
 
-    it('pass `d` should return 月份中的第几天，有前导零的 2 位数字', function (done) {
+    it('should return 月份中的第几天(有前导零的2位数字) when pass `d`', function (done) {
         seajs.use('/project/util/date/js/format', function (format) {
             expect('09').toBe(format('d', timestamp));
+            done();
+        });
+    });
+
+    it('should return 星期中的第几天(3 个字母) when pass `D`', function () {
+        seajs.use('/project/util/date/js/format', function (format) {
+            expect('Wed').toBe(format('D', timestamp));
             done();
         });
     });
