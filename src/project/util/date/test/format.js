df22b12f5175ed2c442f6a0c72c0af0e00cb1124
Hu Jiwei <guyue.jiwei@gmail.com>
2015-03-10 11:11:07 +0800

    添加`i`测试用例

--- src/project/util/date/test/format.js
+++ src/project/util/date/test/format.js
@@ -245,4 +245,15 @@ describe('Util date format module test suite', function () {
             done();
         });
     });
+
+    it('should return 有前导零的分钟数 when pass `i`', function (done) {
+        seajs.use('/project/util/date/js/format', function (format) {
+            // Tue Mar 10 2015 10:04:12 GMT+0800 (CST)
+            expect('04').toBe(format('i', 1425953052671));
+            // Tue Mar 10 2015 00:00:00 GMT+0800 (CST)
+            expect('00').toBe(format('i', 1425916800000));
+            expect('28').toBe(format('i', timestamp));
+            done();
+        });
+    });
 });
