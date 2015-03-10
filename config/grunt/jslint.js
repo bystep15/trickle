3c7a173daef6292e3169122902d7d86717261386
Hu Jiwei <guyue.jiwei@gmail.com>
2015-03-10 13:04:04 +0800

    添加jslint预定义变量

--- config/grunt/jslint.js
+++ config/grunt/jslint.js
@@ -9,10 +9,14 @@ module.exports = {
             nomen: true,        // 允许下划线
             unparam: true,      // 允许未使用的参数
             predef: [
+                'seajs',
                 'define',
                 'console',
                 'alert',
-                'location'
+                'location',
+                'describe',
+                'it',
+                'expect'
             ]
         },
         exclude: [
