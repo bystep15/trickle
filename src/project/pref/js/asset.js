eff10f8a622ef8ecbfe327f6adfdc0db465e276c
Hu Jiwei <guyue.jiwei@gmail.com>
2015-05-10 21:55:19 +0800

    页面静态资源加载质量检测

--- src/project/pref/js/asset.js
+++ src/project/pref/js/asset.js
@@ -1,11 +1,94 @@
 define(function (require, exports, module) {
     'use strict';
 
-    function Asset() {
+    var global = window;
+
+    function Asset(url, key) {
+        if (!url && !key) {
+            throw new Error('请传入完整的url和key参数');
+        }
+        this.url = url;
+        this.key = key;
     }
 
     Asset.prototype = {
-        constructor: Asset
+        constructor: Asset,
+
+        // 采样比例
+        ratio: 10,
+
+        getPerformance: function () {
+            if (!!global.performance) {
+                return global.performance;
+            }
+
+            if (!!global.msPerformance) {
+                return global.msPerformance;
+            }
+
+            if (!!global.webkitPerformance) {
+                return global.webkitPerformance;
+            }
+
+            return null;
+        },
+
+        getEntries: function (performance) {
+            return performance.getEntriesByType('resource');
+        },
+
+        getSampling: function (entries) {
+            return entries.sort(this.compare)[0];
+        },
+
+        stringify: function (sampling) {
+            return JSON.stringify(sampling);
+        },
+
+        compare: function (entry1, entry2) {
+            return entry2.duration - entry1.duration;
+        },
+
+        param: function (url, key, value) {
+            if (url.indexOf('?') >= 0) {
+                url += '&';
+            } else {
+                url += '?';
+            }
+
+            url += encodeURIComponent(key) + '=' + encodeURIComponent(value);
+
+            return url;
+        },
+
+        isLucky: function () {
+            return Math.ceil(Math.random() * 100) <= this.ratio;
+        },
+
+        log: function () {
+            var performance,
+                entries,
+                sampling,
+                image;
+
+            if (!this.isLucky()) {
+                return;
+            }
+
+            performance = this.getPerformance();
+
+            if (!performance) {
+                return;
+            }
+
+            entries = this.getEntries(performance);
+            sampling = this.getSampling(entries);
+
+            if (sampling) {
+                image = new Image();
+                image.url = this.param(this.url, this.key, sampling);
+            }
+        }
     };
 
     module.exports = Asset;
--- src/project/pref/test/assetSpec.js
+++ src/project/pref/test/assetSpec.js
@@ -1,3 +1,90 @@
 describe('Perf Asset Test Suite', function () {
     'use strict';
+
+    function init(Asset, url, key) {
+        url = url || 'http://log.bystep.com';
+        key = key || 'entry';
+            
+        return new Asset(url, key);
+    }
+
+    describe('Constructor Function', function () {
+        it('should return 一个包含log方法的实例', function (done) {
+            seajs.use('/project/pref/js/asset', function (Asset) {
+                var asset = init(Asset);
+
+                expect(asset.log).toEqual(jasmine.any(Function));
+                done();
+            });
+        });
+
+        it('should 抛出错误，当传入的参数不完整的时候', function (done) {
+            seajs.use('/project/pref/js/asset', function (Asset) {
+                function test() {
+                    return new Asset();
+                }
+                expect(test).toThrow();
+                done();
+            });
+        });
+    });
+
+    describe('getEntries', function () {
+        it('should 返回一个当前网页中资源列表的数组', function (done) {
+            seajs.use('/project/pref/js/asset', function (Asset) {
+                var asset = init(Asset),
+                    performance = jasmine.createSpyObj('performance',
+                        ['getEntriesByType']);
+
+                asset.getEntries(performance);
+
+                expect(performance.getEntriesByType).toHaveBeenCalled();
+
+                done();
+            });
+        });
+    });
+
+    describe('getSampling', function () {
+        it('should 返回资源列表中加载时间最长的一个资源', function (done) {
+            seajs.use('/project/pref/js/asset', function (Asset) {
+                var asset = init(Asset),
+                    entries = [
+                        {duration: 300},
+                        {duration: 200},
+                        {duration: 100},
+                        {duration: 500}
+                    ];
+
+                expect(asset.getSampling(entries)).toEqual({duration: 500});
+
+                done();
+            });
+        });
+    });
+
+    describe('param', function () {
+        it('should 将key和value序列化到search参数中', function (done) {
+            seajs.use('/project/pref/js/asset', function (Asset) {
+                var asset = init(Asset);
+
+                expect(asset.param('http://a.com', 'key', 'value')).toBe('http://a.com?key=value');
+                expect(asset.param('http://a.com/?b=c', 'key', 'value')).toBe('http://a.com/?b=c&key=value');
+
+                done();
+            });
+        });
+    });
+
+    describe('isLucky', function () {
+        it('should 返回true或false，标识是否可发送log', function (done) {
+            seajs.use('/project/pref/js/asset', function (Asset) {
+                var asset = init(Asset);
+
+                expect(asset.isLucky()).toEqual(jasmine.any(Boolean));
+
+                done();
+            });
+        });
+    });
 });
