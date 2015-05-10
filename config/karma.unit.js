4a8a7ecb963b694fdb3415562616049b9e9bc37e
Hu Jiwei <guyue.jiwei@gmail.com>
2015-05-10 12:50:56 +0800

    添加ie,firefox,safari,opera 4大浏览器为默认测试浏览器

--- config/karma.unit.js
+++ config/karma.unit.js
@@ -51,7 +51,7 @@ module.exports = function (config) {
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers: ['Chrome', /*'Firefox', 'Safari'*/],
+        browsers: ['IE', 'Chrome', 'Firefox', 'Safari', 'Opera'],
 
 
         // Continuous Integration mode
