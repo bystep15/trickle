1c8a7ee23ffb349648ff934c6c07ed379ad297e6
guyue <guyue.jiwei@gmail.com>
2015-08-01 23:23:21 +0800

    refactor demo

--- src/project/exif/demo/main.js
+++ src/project/exif/demo/main.js
@@ -4,10 +4,11 @@ define(function (require) {
         area = document.querySelector('.drop-area'),
         container = document.querySelector('.thumbnail-container');
 
-    function load(blob) {
+    function load(value) {
         var li = document.createElement('li'),
             img = document.createElement('img'),
             span = document.createElement('span'),
+            blob = value.thumbnail,
             url = URL.createObjectURL(blob);
 
         img.src = url;
@@ -16,6 +17,7 @@ define(function (require) {
             URL.revokeObjectURL(url);
         };
 
+        span.innerHTML = 'Orientation: ' + value.Orientation;
         li.appendChild(img);
         li.appendChild(span);
         container.appendChild(li);
@@ -40,7 +42,7 @@ define(function (require) {
         for (var i = 0, len = files.length; i < len; i += 1) {
             EXIF.getAllTags(files[i], function (value) {
                 console.log(value);
-                load(value.thumbnail);
+                load(value);
             });
         }
     });
